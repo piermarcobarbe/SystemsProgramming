4cab4b447e7134376de9c02a4157c231ee267827
piermarcobarbe <barbep@usi.ch>
2019-03-16 16:27:46 +0100

    How to complete an exercise in 3 lines:

--- shellComments.c
+++ shellComments.c
@@ -26,7 +26,8 @@ int main(int argc, char * argv[]){
     a = arrayListStringRemoveIfMatching("", a);
 
 //    printArrayListStringVerbose(a);
-    printArrayListStringVerbose(a);
+//    printArrayListStringVerbose(a);
 
+    printArrayListStringVerbose(a);
 
 }
