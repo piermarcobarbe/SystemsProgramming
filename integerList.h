dc8f19c026f73826acfa2fb664d5257a2fdc4caf
piermarcobarbe <barbep@usi.ch>
2019-03-16 16:23:57 +0100

    Like floatList, but with integers.

---
+++ integerList.h
@@ -0,0 +1,19 @@
+//
+// Created by Piermarco Barbè on 2019-03-15.
+//
+
+#ifndef SYSTEMSPROGRAMMING_INTEGERLIST_H
+#define SYSTEMSPROGRAMMING_INTEGERLIST_H
+
+
+
+typedef struct integerList {
+    int value;
+    struct * integerList next;
+} integerList;
+
+
+
+
+
+#endif //SYSTEMSPROGRAMMING_INTEGERLIST_H
