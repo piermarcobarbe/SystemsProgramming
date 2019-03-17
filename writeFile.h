70d90103f129d24db6b69170f7810a4877d82fe7
piermarcobarbe <barbep@usi.ch>
2019-03-17 16:03:26 +0100

    Updated header file with corresponding implementations.

--- writeFile.h
+++ writeFile.h
@@ -8,9 +8,11 @@
 #include "arrayListString.h"
 #include <stdio.h>
 
-FILE * openFile(char * filename);
+FILE * openFile(char * filename, char * mode);
 int closeFile(FILE * fp);
-int writeFileCharP(char * filename, char * input);
-int writeFileArrayListString(char * filename, struct arrayListString * a);
-int writeFileChar(char * filename, char input);
+int writeFileCharP(char * filename, char * mode,  char * input);
+int writeFileCharPInt(char * filename, char * mode, char * input, int pos);
+int writeFileChar(char * filename, char * mode, char input);
+int writeFileArrayListString(char * filename, char * mode, struct arrayListString * a);
+int wipeFile(char * filename);
 #endif //SYSTEMSPROGRAMMING_WRITEFILE_H
