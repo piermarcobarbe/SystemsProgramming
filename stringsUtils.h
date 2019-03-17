0d25eef600010729fc26d8aa0141a53d417ee4ab
piermarcobarbe <barbep@usi.ch>
2019-03-17 16:01:50 +0100

    Updated API and new implementations.

--- stringsUtils.h
+++ stringsUtils.h
@@ -5,10 +5,15 @@
 #ifndef SYSTEMSPROGRAMMING_STRINGSUTILS_H
 #define SYSTEMSPROGRAMMING_STRINGSUTILS_H
 
+char * appendChar(char * s, char c);
+char * getSubstringCharPCharP(char * s, char * begin, char * end);
+char * getSusbstringCharP(char * s, char * in);
+char * replaceString(char * s, char * out, char * in);
+char * replaceChar(char * s, char out, char in);
 char * flipLine(char * s);
 char * removeChar(char * s, char c);
 char * removeFirstChar(char * s, char c);
-char * getSubstring(char * s, int begin, int end);
+char * getSubstringIntInt(char * s, int begin, int end);
 int getCharFromString(char * s, char c);
 int deleteDigits(char * s);
 
