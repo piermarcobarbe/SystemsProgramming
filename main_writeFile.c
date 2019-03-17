63018b3857f6135f27bff7d6e300ac49c8efa3df
piermarcobarbe <barbep@usi.ch>
2019-03-17 15:58:15 +0100

    Dummy file for testing writeFile.h

--- main_writeFile.c
+++ main_writeFile.c
@@ -9,16 +9,67 @@
 #include "readFile.h"
 
 int main(){
-    FILE * fp = openFile("./tests");
-
 
     char * targetFilename = "./tests/test3.txt";
+    char * modifiedFilename = "./tests/test3_modified.txt";
+
+    wipeFile(modifiedFilename);
+
     struct arrayListString * a = readFile(targetFilename);
-    fp = openFile(targetFilename);
+    struct arrayListString * l = arrayListStringItemGet(a, 19);
+    char * theString = l->value;
+//    printf("%s\n", theString);
+    theString = replaceString(theString, ". ", ".\n");
+    arrayListStringItemSet(a, 19, theString);
+    writeFileArrayListString(modifiedFilename, "a+", a);
 
-    printArrayListString(a);
+    a = readFile(modifiedFilename);
+    l = arrayListStringItemGet(a, 17);
+    theString = l->value;
+//    printf("theString: %s\n", theString);
+    theString = replaceString(theString, " ", " - ");
+//    printf("theString: %s\n", theString);
+    arrayListStringItemSet(a, 17, theString);
+//    printArrayListString(a);
+    wipeFile(modifiedFilename);
+    struct arrayListString * b = readFile(modifiedFilename);
+//    printArrayListString(b);
+    writeFileArrayListString(modifiedFilename, "a+", a);
 
-//    printf("%p\n", &fp);
+    a = readFile(modifiedFilename);
+    l = arrayListStringItemGet(a, 15);
+    theString = l->value;
+    theString = replaceString(theString, "a", "A");
+    theString = replaceString(theString, "b", "B");
+    theString = replaceString(theString, "c", "C");
+    arrayListStringItemSet(a, 15, theString);
+    wipeFile(modifiedFilename);
+    writeFileArrayListString(modifiedFilename, "a+", a);
+
+    a =readFile(modifiedFilename);
+    l = arrayListStringItemGet(a, 13);
+    theString = l->value;
+    theString = removeChar(theString, ' ');
+    arrayListStringItemSet(a, 13, theString);
+    wipeFile(modifiedFilename);
+    writeFileArrayListString(modifiedFilename, "a+", a);
+
+    a = readFile(modifiedFilename);
+    l = arrayListStringItemGet(a, 11);
+    theString = l->value;
+    theString = getSubstringCharPCharP(theString, "Justice", ".");
+//    printf("ts:%s", theString);
+    theString = appendChar(theString, '\n');
+//    printf("ts:%s", theString);
+    arrayListStringItemSet(a, 11, theString);
+//    printArrayListString(a);
+    wipeFile(modifiedFilename);
+    writeFileArrayListString(modifiedFilename, "a+", a);
+
+
+
+
+
 }
 
 
